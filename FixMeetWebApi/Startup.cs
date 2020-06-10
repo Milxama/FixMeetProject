93a955264a7274ced550b4da9abc6157c87d2c37
ashot <ashot@DESKTOP-LVQ20RG>
2020-06-10 15:09:56 +0300

    open the project

---
+++ FixMeetWebApi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FixMeetWebApi.Startup))]
+namespace FixMeetWebApi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
