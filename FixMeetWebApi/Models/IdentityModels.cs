3014adb46dd13946eeb7e228bc06b5392bc05374
ashot <ashot@DESKTOP-LVQ20RG>
2020-06-12 11:01:02 +0300

    Change user model, and add fields, add migrations and update database, also create new user with new fields, check it in db and create new offer.

--- FixMeetWebApi/Models/IdentityModels.cs
+++ FixMeetWebApi/Models/IdentityModels.cs
@@ -9,6 +9,27 @@ namespace FixMeetWebApi.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        
+        public int UserID { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+      //  public string Email { get; set; }
+       
+        public string ConfirmEmail { get; set; }
+     //   public string UserName { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public string Address { get; set; }
+
+        public UserRole UserRole { get; set; }
+        public Category Category { get; set; }
+
+        public string Radius { get; set; }
+
+
+
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
