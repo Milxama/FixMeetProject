5781f33372ce70f3ec96c3ebd16b5a8e4f43d132
YT <y.krichely@gmail.com>
2020-06-21 10:59:00 +0300

    updated request index

--- FixMeetWebApi/Controllers/OfferModelsController.cs
+++ FixMeetWebApi/Controllers/OfferModelsController.cs
@@ -18,43 +18,24 @@ namespace FixMeetWebApi.Controllers
         private ApplicationDbContext db = new ApplicationDbContext();
 
         // GET: OfferModels
-        public ActionResult Index()
-        {
-            var user_id = User.Identity.GetUserId();
-            var user = db.Users.Where(u => u.Id == user_id).FirstOrDefault();
-
-            var userRole = db.Users.Where(u => u.Id == user_id).FirstOrDefault().UserRole;
-
-
-            if (userRole == UserRole.Supplier)
-            {
-                var offer = db.OfferModels.Where(off => off.UserID == user_id).ToList();
-              //  var request_id = offer.LastOrDefault().RequestID;
-               // var offerList = offer.Where(o => o.RequestID == request_id).ToList();
-                return View(offer);
-            }
-            return View(db.OfferModels.ToList());
-
-        }
-
-        // GET: OfferModels
-        [HttpPost]
         public ActionResult Index(int? requestId)
         {
             var user_id = User.Identity.GetUserId();
             var user = db.Users.Where(u => u.Id == user_id).FirstOrDefault();
-           
+
             var userRole = db.Users.Where(u => u.Id == user_id).FirstOrDefault().UserRole;
-            
 
-
+            if (userRole == UserRole.Supplier)
+            {
+                var offer = db.OfferModels.Where(off => off.UserID == user_id).ToList();
+                return View(offer);
+            }
             if (userRole == UserRole.Customer)
             {
-                var offer = db.OfferModels.Where(off => off.RequestID == requestId && off.UserID == user_id).ToList();
+                var offer = db.OfferModels.Where(off => off.RequestID == requestId).ToList();
                 return View(offer);
 
             }
-
 
             return View(db.OfferModels.ToList());
         }
